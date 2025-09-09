0d8823b67240a5f045a9d02df654c91767c53978
LAPTOP-0RUBD428\XLOC <tathankochi@gmail.com>
2025-09-09 14:23:27 +0700

    feat(auth): implement JWT and local authentication strategies
    - Add Passport module and guards for JWT and local strategies
    - Update AuthService to handle user validation and login
    - Modify AuthController to use new guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { LocalAuthGuard } from './passport/local-auth.guard';
+import { JwtAuthGuard } from './passport/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post("login")
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.signIn(
-      createAuthDto.username,
-      createAuthDto.password
-    )
+  @UseGuards(LocalAuthGuard)
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,19 +10,21 @@ export class AuthService {
     private jwtService: JwtService
   ) { }
 
-  async signIn(username: string, pass: string): Promise<any> {
+  async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
     if (user) {
       const isValidPassword = await comparePasswordHelper(pass, user.password);
-      if (!isValidPassword) {
-        throw new UnauthorizedException("Username/Password không hợp lệ.");
+      if (isValidPassword) {
+        return user
       }
-
-      const payload = { sub: user._id, username: user.email };
-      return {
-        access_token: await this.jwtService.signAsync(payload),
-      };
     }
-    throw new UnauthorizedException("Username/Password không hợp lệ.");
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
